73c7e2facea2bc6b7035255472a709f374d77099
Spencer Blackwell <spencerblackwell02@gmail.com>
2024-02-19 17:02:52 -0600

    checking some stuff

--- task2/main.c
+++ task2/main.c
@@ -2,16 +2,22 @@
 #include <dirent.h>
 #include <string.h>
 #include <ctype.h>
+#include <time.h>
 
 //Using this library to check get the directory entry:
 // https://pubs.opengroup.org/onlinepubs/7908799/xsh/dirent.h.html
 
+//proc stuff from here:
+// https://docs.kernel.org/filesystems/proc.html
+
+//I feel like there's gotta be a better way to do this
 int isProcess(const struct dirent *proc){
     //to look for a number, we can iterate over the possible processes, or proc
-    printf("Iterating");
+    printf(proc + "\n")
     for(int i = 0; i < strlen(proc->d_name);i++){
         //checks if the current char is a digit
         if(!isdigit(proc->d_name[i])){
+            printf(d_name[i] + "\n")
             return 0;
         }
     }
