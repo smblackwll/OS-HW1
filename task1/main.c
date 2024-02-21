375c646cf71ed871816a01022c385d741b82d76a
Spencer Blackwell <spencerblackwell02@gmail.com>
2024-02-20 22:40:04 -0600

    First pass at reading in from user

---
+++ task1/main.c
@@ -0,0 +1,34 @@
+//Major uses here:
+//execvp is used to execute c code - we can essentially just parse the input string from the user, check if the command is exit
+// if it's exit run the exit function. Otherwise, we can use execvp and the parsed args to run a command.
+//we may need fork?
+#include <stdio.h>
+
+// Reading in from stdin safely stuff here, just reads it all in as one big string.
+// https://alexandra-zaharia.github.io/posts/how-to-read-safely-from-stdin-in-c/
+void main(void){
+    int run = 1;
+    char str[256];
+    char userIn[256];
+    char args[256];
+
+    while(run = 1){
+        char userIn = fgets(str, sizeof(str), stdin);
+        if(!userIn){
+            return;
+        }
+        while (str[i] != '\n' && str[i] != '\0')
+            i++;
+
+        if (str[i] == '\n'){
+            str[i] = '\0';
+
+        }
+
+        printf(str);
+
+        if(strcmp(str, "exit()") == 0){
+            run = 0;
+        }
+    }
+}
